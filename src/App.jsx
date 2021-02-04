81d428f88bc7f124c683cf7ddd522ce6994709a1
Darius Șincu <darius.sincu@pitechnologies.ro>
2021-02-04 15:40:37 +0200

    Add routing and update menu-item to navigate withRouter HOC

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import "./App.css";
-import HomePage from "./pages/homepage/homepage.component.jsx";
+import HomePage from './pages/homepage/homepage.component.jsx';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+  <>
+    <h1>HATS PAGE</h1>
+  </>
+);
+
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </>
   );
 }
 
