fc6b2da58dbd95c0109732432e2ab7c584b993dd
sincudarius <40241019+sincudarius@users.noreply.github.com>
2021-02-05 15:22:54 +0200

    Adding header menu and shop and components links (#5)
    * Adding header menu and shop and components links
    
    * Comments resolved
    
    Co-authored-by: Darius Șincu <darius.sincu@pitechnologies.ro>

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/home/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 import './App.css';
 
 function App() {
   return (
     <>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
