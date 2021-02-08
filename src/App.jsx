fc11b09b81ab52fa8dd406ca8c47cef2526752c2
Darius Șincu <darius.sincu@pitechnologies.ro>
2021-02-08 17:04:23 +0200

    Sign in and sign out with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/home/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-sing-up';
 import Header from './components/header/header.component';
 
 import './App.css';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-      </Switch>
-    </>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      console.log(user);
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUp} />
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
