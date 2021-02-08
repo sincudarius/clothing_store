1d13e78f8d57389cd9c7f3932cb9817a6364eca8
sincudarius <40241019+sincudarius@users.noreply.github.com>
2021-02-08 17:23:18 +0200

    Develop to main (#7)
    * Add routing and update menu-item to navigate withRouter HOC
    
    * Comments resolved
    
    * create shop page, collection preview and collection item (#4)
    
    * create shop page, collection preview and collection item
    
    * Comments resolved
    
    Co-authored-by: Darius Șincu <darius.sincu@pitechnologies.ro>
    
    * Adding header menu and shop and components links (#5)
    
    * Adding header menu and shop and components links
    
    * Comments resolved
    
    Co-authored-by: Darius Șincu <darius.sincu@pitechnologies.ro>
    
    * Sign in and sign out with firebase (#6)
    
    * Sign in and sign out with firebase
    
    * Comments resolved
    
    Co-authored-by: Darius Șincu <darius.sincu@pitechnologies.ro>
    
    Co-authored-by: Darius Șincu <darius.sincu@pitechnologies.ro>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
-import "./App.css";
-import HomePage from "./pages/homepage/homepage.component.jsx";
-function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import HomePage from './pages/home/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-sing-up';
+
+import { auth } from './firebase/firebase.utils';
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      console.log(user);
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUp} />
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
